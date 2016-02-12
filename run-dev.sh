f9dcbcf6579253523f86801787ac22b2f8f81d88
Kristofor Carle <kristoforcarle@moabi.org>
2016-02-11 22:32:21 -0500

    remove hosts file hack and add dev env host via docker run

--- run-dev.sh
+++ run-dev.sh
@@ -2,4 +2,4 @@
 docker stop manet
 docker rm manet
 docker build --tag="manet" .
-docker run --name manet -p 8891:8891 -d manet
+docker run --name manet -p 8891:8891 --add-host=dev.localhost:192.168.59.1 -d manet
